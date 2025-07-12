1bb8b0473be00bae01de4b464b0092798c5745df
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2025-07-12 11:34:24 +0600

    path added in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,14 @@ import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Cart from '@/pages/Cart';
+import Checkout from '@/pages/Checkout';
+import OrderTracking from '@/pages/OrderTracking';
+import Profile from '@/pages/Profile';
+import Orders from '@/pages/Orders';
 import RestaurantRegistration from './pages/RestaurantRegistration';
+import Restaurants from '@/pages/Restaurants';
+import RestaurantDetail from '@/pages/RestaurantDetail';
+import RestaurantDashboard from '@/pages/RestaurantDashboard';
 import { Toaster } from './components/ui/sonner';
 
 function App() {
@@ -18,9 +25,16 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path='/restaurantRegister' element={<RestaurantRegistration />} />
+            <Route path="/restaurant/dashboard" element={<RestaurantDashboard />} />
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
+              <Route path="restaurants" element={<Restaurants />} />
+              <Route path="restaurant/:id" element={<RestaurantDetail />} />
               <Route path="cart" element={<Cart />} />
+              <Route path="checkout" element={<Checkout />} />
+              <Route path="order/:id" element={<OrderTracking />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="orders" element={<Orders />} />
             </Route>
           </Routes>
         </Router>
